f970ef2c0353d81e712df0b37a37c9abcdf70d89
System Administrator <root@MacBook-Pro-de-usuario.local>
2018-12-03 14:46:59 -0500

    se realizan cambios en kanban y se elimina la dependencia de account.accounting

--- __manifest__.py
+++ __manifest__.py
@@ -2,19 +2,20 @@
 {
     'name': 'Colombia - Terceros',
     'category': 'Localization',
-    'version': '10.01',
+    'version': '11.01',
     'author': 'Dominic Krimmer, Plastinorte S.A.S, Odoo LoCo',
     'license': 'AGPL-3',
     'maintainer': 'dominic.krimmer@gmail.com',
     'website': 'https://www.plastinorte.com',
     'summary': 'Colombia Terceros: Extended Partner / '
-               'Contact Module - Odoo 10.0',
+               'Contact Module - Odoo 11.0',
     'images': ['images/main_screenshot.png'],
     'depends': [
         'account',
-        'account_accountant',
         'base'
     ],
-    'data': [],
+    'data': [
+        'views/l10n_co_res_partner.xml'
+    ],
     'installable': True,
 }
